92f300bf90b677f4e63e323d7f2f072bde3da795
Aresdgi <115067719+Aresdgi@users.noreply.github.com>
2024-03-15 17:01:55 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "one"];
+  let adjectives = ["cool", "newbie", "bold", "absurd"];
+  let nouns = ["developer", "spirit", "brain"];
+  let extensions = [".tech", ".pro"];
+
+  let allArrays = [pronouns, adjectives, nouns, extensions];
+  let domains = allArrays.reduce(function totalCombinations(total, arr) {
+    return total * arr.length;
+  }, 1);
+
+  let allDomains = [];
+
+  for (let i = 0; i < domains; i++) {
+    let innerIndex = i;
+    let combined = allArrays
+      .map(function totalCombinations(arr) {
+        let index = innerIndex % arr.length;
+        innerIndex = Math.floor(i / arr.length);
+        return arr[index];
+      })
+      .join("");
+
+    allDomains.push(combined);
+  }
+  document.querySelector("#domainGenerator").innerHTML = allDomains.join(
+    "<br>"
+  );
 };
